2311950dee1b19e183c3c2b6e2d0a98130a548fc
talson <talsonservice@gmail.com>
2022-07-04 11:44:09 +0300

    Secured API keys

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -1,13 +1,12 @@
 import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+require('dotenv').config();
 
 export default defineConfig({
   shopify: {
     defaultCountryCode: 'US',
     defaultLanguageCode: 'EN',
     storeDomain: 'aliencafe.myshopify.com',
-    storefrontToken: 'c6a397cdf9506e7495a51e087adf9b64',
-    // apiKey: '7d701b783941151fca6e5b7ce86062f0',
-    // apiSecretKey: '7ac807431979727903367255880a0f2b',
+    storefrontToken: process.env.storefrontToken,
     storefrontApiVersion: '2022-07',
   },
   session: CookieSessionStorage('__session', {
